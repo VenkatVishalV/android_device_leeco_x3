3154a27d14137929673b5a2c7a0f88e730f48ff0
VenkatVishalV <31775889+VenkatVishalV@users.noreply.github.com>
2017-09-18 19:36:11 +0530

    Add files via upload

---
+++ patches/install.sh
@@ -0,0 +1,17 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="bionic frameworks/av frameworks/base system/core system/netd"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/device/leeco/x3/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
