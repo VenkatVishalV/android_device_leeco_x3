25d9b4e0bfdd4b238d93470d1fe3d844f25c1555
Venkat Vishal V <venkatvishal124@gmail.com>
2017-10-16 16:50:04 +0530

    Added Camera2 Support

--- device.mk
+++ device.mk
@@ -308,3 +308,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
+#camera2
+PRODUCT_PACKAGES += \
+camera2
