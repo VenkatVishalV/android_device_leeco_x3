b30a7d47e2c21b316e767717365ee0d40709beee
Venkat Vishal V <venkatvishal124@gmail.com>
2017-10-19 13:07:28 +0530

    Fix a small Error!

--- device.mk
+++ device.mk
@@ -308,9 +308,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
  
-#camera2
+#Camera2
 PRODUCT_PACKAGES += \
-camera2
+Camera2
  
  # USB
  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
