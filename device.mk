308a98cb09e21a61214dcbdcfc0c49fe6ad3e502
Venkat Vishal V <venkatvishal124@gmail.com>
2017-10-19 13:04:09 +0530

    Fix a small error!

--- device.mk
+++ device.mk
@@ -308,7 +308,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
     
-#camera2
+#Camera2
 PRODUCT_PACKAGES += \
-camera2
+Camera2
 
