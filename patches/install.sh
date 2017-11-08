fc3737335a77f9873d5e6bebbe49416978d71889
Venkat Vishal V <venkatvishal124@gmail.com>
2017-11-08 18:56:43 +0530

    Update install.sh

--- patches/install.sh
+++ patches/install.sh
@@ -1,24 +1,16 @@
-#!/bin/sh
-
 echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="build/make/core build/soong hardware/interfaces system/core bionic frameworks/av frameworks/base"
-
-# red + nocolor
-RED='\033[0;31m'
-NC='\033[0m'
-
+dirs="bionic build/maake/core build/soong frameworks/av frameworks/base hardware/interfaces system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
-        echo -e "\n${RED}Applying ${NC}$dir ${RED}patches...${NC}\n"
-	#git am $rootdirectory/device/leeco/x3/$dir/*.patch
+        echo "Applying $dir patches..."
 	git apply -v $rootdirectory/device/leeco/x3/patches/$dir/*.patch
 done
 
 # -----------------------------------
-echo -e "Done !\n"
+echo "Changing to build directory..."
 cd $rootdirectory
